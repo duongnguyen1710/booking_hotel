06a87b0389d43f6b2ea5c89f7b2363e9b20d0cec
duongnguyen1710 <nguyenduong171002@gmail.com>
2025-02-05 17:16:47 +0700

    dang nhap, dang ky khach hang

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -8,29 +8,25 @@
                         <div class="logo">
                             <a href="index.html" style="font-size: 32px; font-weight: bold; text-transform: uppercase; color: black; text-decoration: none;">BookingHotel</a>
                         </div>
-
                     </div>
                 </div>
             </div>
             <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
-                <nav class="navigation navbar navbar-expand-md navbar-dark ">
+                <nav class="navigation navbar navbar-expand-md navbar-dark d-flex align-items-center">
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarsExample04">
-                        <ul class="navbar-nav mr-auto">
+                    <div class="collapse navbar-collapse d-flex align-items-center" id="navbarsExample04">
+                        <ul class="navbar-nav">
                             <li class="nav-item active">
                                 <a class="nav-link" href="index.html">Trang chủ</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="about.html">Dịch vụ của chúng tôi</a>
+                                <a class="nav-link" href="about.html">Chi tiết</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="room.html">Chọn phòng</a>
                             </li>
-                            <!-- <li class="nav-item">
-                                <a class="nav-link" href="gallery.html">Gallery</a>
-                            </li> -->
                             <li class="nav-item">
                                 <a class="nav-link" href="blog.html">Tin tức</a>
                             </li>
@@ -38,9 +34,43 @@
                                 <a class="nav-link" href="contact.html">Liên hệ</a>
                             </li>
                         </ul>
+
+                        <div class="auth-nav ml-auto d-flex align-items-center">
+                            @if (Route::has('login'))
+                            @auth
+                            <div class="dropdown">
+                                <button class="btn btn-secondary dropdown-toggle" type="button" id="userMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    {{ Auth::user()->name }}
+                                </button>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userMenu">
+                                    <a class="dropdown-item" href="{{ route('profile.show') }}">Hồ sơ</a>
+                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                        Đăng xuất
+                                    </a>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </div>
+                            @else
+                            <a class="btn btn-success mr-2" href="{{ url('login') }}">Đăng nhập</a>
+
+                            @if (Route::has('register'))
+                            <a class="btn btn-primary" href="{{ url('register') }}">Đăng ký</a>
+                            @endif
+                            @endauth
+                            @endif
+                        </div>
                     </div>
                 </nav>
             </div>
         </div>
     </div>
 </div>
+
+<style>
+    .auth-nav {
+        position: relative;
+        top: -12px;
+    }
+</style>
